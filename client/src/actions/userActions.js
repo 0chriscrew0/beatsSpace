50c6cd5c627599b74ee0a5f6741e83df95c8579b
Christopher Crow <0chriscrew0@gmail.com>
2019-03-04 16:02:40 -0600

    setup redux

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+import { USER_ROUTES } from "../utils/misc";
--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,29 @@ import "./resources/css/main.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import PromiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+import Reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+  PromiseMiddleware,
+  ReduxThunk
+)(createStore);
+
 ReactDOM.render(
-  <Router>
-    <Routes />
-  </Router>,
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <Router>
+      <Routes />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
