d38606d3d47739d6dc6eba6d8201e21172fd8e56
Christopher Crow <0chriscrew0@gmail.com>
2019-04-08 14:21:01 -0500

    fix image layout styles in shop component

--- client/src/components/Utils/ProductCard.js
+++ client/src/components/Utils/ProductCard.js
@@ -3,25 +3,23 @@ import { Link } from "react-router-dom";
 
 import DefaultImage from "../../resources/img/featured-image-01.jpg";
 
-const ProductCard = ({ type, _id, images, name, price }) => {
+const ProductCard = ({ _id, images, name, price }) => {
   return (
     <div className={`card product-card`}>
-      <img
-        className={`card-img-top img-fluid product-card__img`}
-        src={images ? images[0].url : DefaultImage}
-        alt="beat"
-      />
+      <div className="card-img-wrapper bg-white">
+        <img
+          className={`card-img-top img-fluid product-card__img`}
+          src={images ? images[0].url : DefaultImage}
+          alt="beat"
+        />
+      </div>
+
       <div className="card-body d-flex flex-column justify-content-between">
         <h5 className="product-title">
           <div>{name}</div>
           <div>{`$${price}`}</div>
         </h5>
 
-        {/* <div className="product-card-preview">
-          <button className="btn btn-sm btn-default">
-            <i className="fas fa-play" />
-          </button>
-        </div> */}
         <div className="product-card-buttons">
           <Link className="btn btn-info" to={`/product/${_id}`}>
             Details
