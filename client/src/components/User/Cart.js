7891f440358afdc2a0ca25b7ba13e90f7ac8b27b
Christopher Crow <0chriscrew0@gmail.com>
2019-05-06 15:40:46 -0500

    fix styling on cart component

--- client/src/components/User/Cart.js
+++ client/src/components/User/Cart.js
@@ -50,34 +50,38 @@ class Cart extends Component {
     return (
       <div className="cart">
         <div className="container">
-          <div className="row flex-md-row-reverse">
-            <div className="col-xs-12 col-md-5 cart-info">
+          <div className="row">
+            <div className="col-xs-12 col-md-7 cart-items">
               <h4 className="cart-header">My Cart</h4>
+              {this.state.loading ? (
+                <Spinner />
+              ) : (
+                <div>{this.renderCartItems()}</div>
+              )}
+            </div>
+            <div className="col-xs-12 col-md-4 py-4 cart-info">
               <p className="cart-quantity mb-3">
-                ({this.props.user.userData.cart.length} Items):
+                Subtotal ({this.props.user.userData.cart.length}{" "}
+                {this.props.user.userData.cart.length === 1 ? "Item" : "Items"}
+                ):
               </p>
               <p className="cart-subtotal lead ml-2 mb-3 text-danger">
                 ${this.calculateSubtotal()}
               </p>
-              <div className="mb-4">
-                <span>Still looking?</span>
-                <Link to="/shop" className="btn btn-sm btn-outline-info ml-2">
-                  Go to Store
-                </Link>
-              </div>
+
               <Link
                 to="/user/checkout"
                 className="btn btn-block btn-primary mb-3"
               >
                 Checkout
               </Link>
-            </div>
-            <div className="col-xs-12 col-md-7 cart-items">
-              {this.state.loading ? (
-                <Spinner />
-              ) : (
-                <div>{this.renderCartItems()}</div>
-              )}
+
+              <hr />
+
+              <span>Still looking?</span>
+              <Link to="/shop" className="btn btn-sm btn-outline-info ml-2">
+                Go to Store
+              </Link>
             </div>
           </div>
         </div>
