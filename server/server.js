6f0d8c0154774d804d3885df5cc3841399b3551c
Christopher Crow <0chriscrew0@gmail.com>
2019-06-05 15:16:46 -0500

    make changes to package.json for production

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 mongoose.Promise = global.Promise;
 mongoose.set("useCreateIndex", true);
 mongoose.set("useFindAndModify", false);
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
